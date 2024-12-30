0185b601eb6d6267a54665eefc7a73b55c94c373
Rodrigo Vittori <vittorirodrigo@gmail.com>
2024-12-30 19:29:37 -0300

    Proyecto publicado
    Incluímos el link a nuestro proyecto en el HUB de Kodland y agregamos TODOS los comentarios que describen el proceso de cómo programamos este juego :)

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
 """
 Primera Actividad Módulo 3: Proyecto - Piedra-Papel-Tijeras
-Versión: 1.0.0
+Versión: 1.0.1
 
 Link al repo: https://github.com/rodrigovittori/Piedra-Papel-Tijera-4499/blob/main/main.py
 Hitorial del repo: https://github.com/rodrigovittori/Piedra-Papel-Tijera-4499/commits/main/main.py
-Link al proyecto final (en HUB): ** todavía NO publicado **
+Link al proyecto final (en HUB): https://hub.kodland.org/project/337200
 -------------------------------------
+
+REGLAS:
+
+Las reglas son sencillas: Los jugadores eligen simultáneamente uno de los tres signos: piedra, papel o tijera. El ganador se determina según las siguientes reglas: 
+
+1.    El papel le gana a la piedra (“El papel envuelve a la piedra”).
+
+2.    La piedra le gana a la tijera (“La piedra destruye a la tijera”).
+
+3.    La tijera le gana al papel (“La tijera corta el papel)
+
+
+Paso Nº 1: Creamos variables: 
+    > contadoras: ronda_actual; puntaje_jugador, puntaje_compu y empates;
+
+Paso Nº 2: Creamos el bucle principal:
+    > agregamos una nueva variable de control, de tipo bool (verdadero o falso) llamada seguir_jugando
+    > agregamos un bucle while(seguir_jugando)
+      y dentro de éste un check para validar si el usuario desea continuar o no
+
+Paso Nº 3: Pedir al Jugador su elección (piedra, papel o tijera)
+    > Creamos una nueva variable (dentro del bucle) -> opcion_jugador
+    > Pedimos su valor con input() y lo convertimos a int()
+        > 1) Piedra
+        > 2) Tijeras
+        > 3) Papel
+
+    > Creamos un bucle para verificar que la opción ingresada sea válida (opcion_jugador >= 0) and (opcion_jugador <= 3)
+        > Si el valor NO es válido, lo solicitamos nuevamente
+        > En cambio, si es válido, convertimos ese valor a su equivalente en texto:
+            > 1: "Piedra" / 2: "Tijeras" / 3: "Papel"
+
+Paso Nº 4: Generamos con random.randint(1-3) la respuesta de la computadora y la almacenamos en una nueva variable
+           llamada opcion_compu; luego, la reemplazamos por el texto equivalente
+
+Paso Nº 5: Evaluamos el resultado de la ronda. La consigna indica que debemos seguir el órden:
+            > empate -> victoria (jugador) -> else: derrota (jugador)
 
 Pasos Nº 6-9: Paso 6: Sumar puntajes en cada ronda según el resultado
                       > agregamos contador += 1 en cada caso de nuestro if-elif-else
